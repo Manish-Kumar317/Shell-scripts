1f0794b95dec7f8856d22009dcaa205be7921f2d
Manish Kumar <317manishkumar@gmail.com>
2019-12-08 16:49:19 +0530

    Update poststart.sh

--- poststart.sh
+++ poststart.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-source_path="/tmp/bootstrap"
+source_path="/f/Wipro/bootstrap"
+dest_path="/f/Wipro/copy"
+
+check_file(){
+	echo "Check File"
+	file=$1
+	if [ ! -f "$source_path/$file" ]
+	then
+    	echo "$0: File '${file}' not found."
+    	exit 0
+	fi
+}
+
+
+#Execute Function
 execute(){
 echo "Execute Function"
 setup_scripts=$1
@@ -8,8 +22,17 @@ IFS=',' read -r -a scripts <<< "$setup_scripts"
 for script in "${scripts[@]}"
 do
     echo "$source_path/$script"
+    check_file $script
+    sh $source_path/$script
+    if [ $? == 0 ]
+    then
+    	echo "Script $script has been executed successfully"
+    else
+    	echo  "Script $script execution failed"
+    fi  	
 done
 }
+
 copy(){
 echo "Copy Function"
 copy_scripts=$1
@@ -17,6 +40,14 @@ IFS=',' read -r -a files <<< "$copy_scripts"
 for file in "${files[@]}"
 do
     echo "$source_path/$file"
+    check_file $file
+    cp $source_path/$file $dest_path/$file
+    if [ $? == 0 ]
+    then
+    	echo "File $file has been copied successfully"
+    else
+    	echo  "File $file copy failed"
+    fi
 done 
 }
 
@@ -30,7 +61,7 @@ do
 	       ifastbase)
 		        
               echo "$element"
-              execute "presetup_ifastbase,setup-ifastbase"
+              execute "presetup_ifastbase,setup_ifastbase"
               copy "gensftp,gensftp.exp"
 		          ;;
 	       desktop)
@@ -41,8 +72,22 @@ do
 	       fundservfiles)
 		          
               echo "$element"
+              execute "setup_fundservfiles"
               
 		          ;;
+	       fundservtrading)
+		          
+              echo "$element"
+              execute "setup_fundservtrading"
+              
+		          ;;
+	       swifttrading)
+		          
+              echo "$element"
+              execute "setup_fundservfilesgit"
+              
+		          ;;
+
 	       sftp)
 		          
               echo "$element"
